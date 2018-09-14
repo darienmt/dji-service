764fa6abee3013ce0849b880d9c99af8c5359575
Darien Martinez <darien.martinez@codekeepersinc.com>
2018-09-14 02:59:34 -0400

    Connecting to the drone/controller

--- app/src/main/java/com/codekeepersinc/djiservice/MApplication.java
+++ app/src/main/java/com/codekeepersinc/djiservice/MApplication.java
@@ -10,9 +10,21 @@ import android.util.Log;
 import com.secneo.sdk.Helper;
 
 public class MApplication extends Application {
+
+    protected DJIServiceApplication application = null;
     @Override
     protected void attachBaseContext(Context paramContext) {
         super.attachBaseContext(paramContext);
         Helper.install(MApplication.this);
+        if ( this.application == null ) {
+            this.application = new DJIServiceApplication();
+            this.application.setContext(this);
+        }
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        this.application.onCreate();
     }
 }
--- app/src/main/java/com/codekeepersinc/djiservice/MainActivity.java
+++ app/src/main/java/com/codekeepersinc/djiservice/MainActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
+import android.graphics.SurfaceTexture;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
@@ -14,17 +15,32 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
+import android.view.TextureView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 
+import dji.common.camera.SettingsDefinitions;
+import dji.common.camera.SystemState;
 import dji.common.error.DJIError;
 import dji.common.error.DJISDKError;
+import dji.common.product.Model;
+import dji.common.useraccount.UserAccountState;
+import dji.common.util.CommonCallbacks;
 import dji.sdk.base.BaseComponent;
 import dji.sdk.base.BaseProduct;
+import dji.sdk.camera.Camera;
+import dji.sdk.camera.VideoFeeder;
+import dji.sdk.codec.DJICodecManager;
 import dji.sdk.sdkmanager.DJISDKManager;
+import dji.sdk.useraccount.UserAccountManager;
 
 /**
  * Skeleton of an Android Things activity.
@@ -53,156 +69,323 @@ import dji.sdk.sdkmanager.DJISDKManager;
 //        setContentView(R.layout.activity_main);
 //    }
 //}
-public class MainActivity extends Activity {
+public class MainActivity extends Activity  implements TextureView.SurfaceTextureListener,View.OnClickListener {
     private static final String TAG = MainActivity.class.getName();
-    public static final String FLAG_CONNECTION_CHANGE = "dji_sdk_connection_change";
-    private static BaseProduct mProduct;
-    private Handler mHandler;
-    private static final String[] REQUIRED_PERMISSION_LIST = new String[]{
-//            Manifest.permission.VIBRATE,
-            Manifest.permission.INTERNET,
-            Manifest.permission.ACCESS_WIFI_STATE,
-            Manifest.permission.WAKE_LOCK,
-//            Manifest.permission.ACCESS_COARSE_LOCATION,
-            Manifest.permission.ACCESS_NETWORK_STATE,
-//            Manifest.permission.ACCESS_FINE_LOCATION,
-            Manifest.permission.CHANGE_WIFI_STATE,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE,
-            Manifest.permission.BLUETOOTH,
-            Manifest.permission.BLUETOOTH_ADMIN,
-            Manifest.permission.READ_EXTERNAL_STORAGE,
-            Manifest.permission.READ_PHONE_STATE,
-    };
-    private List<String> missingPermission = new ArrayList<>();
-    private AtomicBoolean isRegistrationInProgress = new AtomicBoolean(false);
-    private static final int REQUEST_PERMISSION_CODE = 12345;
+    protected VideoFeeder.VideoDataCallback mReceivedVideoDataCallBack = null;
+
+    // Codec for video live view
+    protected DJICodecManager mCodecManager = null;
+
+    protected TextureView mVideoSurface = null;
+    private Button mCaptureBtn, mShootPhotoModeBtn, mRecordVideoModeBtn;
+    private ToggleButton mRecordBtn;
+    private TextView recordingTime;
+
+    private Handler handler;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // When the compile and target version is higher than 22, please request the following permission at runtime to ensure the SDK works well.
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            checkAndRequestPermissions();
-        }
         setContentView(R.layout.activity_main);
-        //Initialize DJI SDK Manager
-        mHandler = new Handler(Looper.getMainLooper());
-    }
-    /**
-     * Checks if there is any missing permissions, and
-     * requests runtime permission if needed.
-     */
-    private void checkAndRequestPermissions() {
-        // Check for permissions
-        for (String eachPermission : REQUIRED_PERMISSION_LIST) {
-            if (ContextCompat.checkSelfPermission(this, eachPermission) != PackageManager.PERMISSION_GRANTED) {
-                missingPermission.add(eachPermission);
-            }
-        }
-        // Request for missing permissions
-        if (missingPermission.isEmpty()) {
-            startSDKRegistration();
-        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            showToast("Need to grant the permissions!");
-            ActivityCompat.requestPermissions(this,
-                    missingPermission.toArray(new String[missingPermission.size()]),
-                    REQUEST_PERMISSION_CODE);
-        }
-    }
-    /**
-     * Result of runtime permission request
-     */
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           @NonNull String[] permissions,
-                                           @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        // Check for granted permission and remove from missing list
-        if (requestCode == REQUEST_PERMISSION_CODE) {
-            for (int i = grantResults.length - 1; i >= 0; i--) {
-                if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
-                    missingPermission.remove(permissions[i]);
+
+        handler = new Handler();
+
+        initUI();
+
+        // The callback for receiving the raw H264 video data for camera live view
+        mReceivedVideoDataCallBack = new VideoFeeder.VideoDataCallback() {
+
+            @Override
+            public void onReceive(byte[] videoBuffer, int size) {
+                if (mCodecManager != null) {
+                    mCodecManager.sendDataToDecoder(videoBuffer, size);
                 }
             }
-        }
-        // If there is enough permission, we will start the registration
-        if (missingPermission.isEmpty()) {
-            startSDKRegistration();
-        } else {
-            showToast("Missing permissions!!!");
+        };
+
+        Camera camera = DJIServiceApplication.getCameraInstance();
+
+        if (camera != null) {
+
+            camera.setSystemStateCallback(new SystemState.Callback() {
+                @Override
+                public void onUpdate(SystemState cameraSystemState) {
+                    if (null != cameraSystemState) {
+
+                        int recordTime = cameraSystemState.getCurrentVideoRecordingTimeInSeconds();
+                        int minutes = (recordTime % 3600) / 60;
+                        int seconds = recordTime % 60;
+
+                        final String timeString = String.format("%02d:%02d", minutes, seconds);
+                        final boolean isVideoRecording = cameraSystemState.isRecording();
+
+                        MainActivity.this.runOnUiThread(new Runnable() {
+
+                            @Override
+                            public void run() {
+
+                                recordingTime.setText(timeString);
+
+                                /*
+                                 * Update recordingTime TextView visibility and mRecordBtn's check state
+                                 */
+                                if (isVideoRecording){
+                                    recordingTime.setVisibility(View.VISIBLE);
+                                }else
+                                {
+                                    recordingTime.setVisibility(View.INVISIBLE);
+                                }
+                            }
+                        });
+                    }
+                }
+            });
+
         }
     }
 
-    private void startSDKRegistration() {
-        if (isRegistrationInProgress.compareAndSet(false, true)) {
-            AsyncTask.execute(new Runnable() {
+    protected void onProductChange() {
+        initPreviewer();
+    }
+
+    @Override
+    public void onResume() {
+        Log.e(TAG, "onResume");
+        super.onResume();
+        initPreviewer();
+        onProductChange();
+
+        if(mVideoSurface == null) {
+            Log.e(TAG, "mVideoSurface is null");
+        }
+    }
+
+    @Override
+    public void onPause() {
+        Log.e(TAG, "onPause");
+        uninitPreviewer();
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        Log.e(TAG, "onStop");
+        super.onStop();
+    }
+
+    public void onReturn(View view){
+        Log.e(TAG, "onReturn");
+        this.finish();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.e(TAG, "onDestroy");
+        uninitPreviewer();
+        super.onDestroy();
+    }
+
+    private void initUI() {
+        // init mVideoSurface
+        mVideoSurface = (TextureView)findViewById(R.id.video_previewer_surface);
+
+        recordingTime = (TextView) findViewById(R.id.timer);
+        mCaptureBtn = (Button) findViewById(R.id.btn_capture);
+        mRecordBtn = (ToggleButton) findViewById(R.id.btn_record);
+        mShootPhotoModeBtn = (Button) findViewById(R.id.btn_shoot_photo_mode);
+        mRecordVideoModeBtn = (Button) findViewById(R.id.btn_record_video_mode);
+
+        if (null != mVideoSurface) {
+            mVideoSurface.setSurfaceTextureListener(this);
+        }
+
+        mCaptureBtn.setOnClickListener(this);
+        mRecordBtn.setOnClickListener(this);
+        mShootPhotoModeBtn.setOnClickListener(this);
+        mRecordVideoModeBtn.setOnClickListener(this);
+
+        recordingTime.setVisibility(View.INVISIBLE);
+
+        mRecordBtn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    startRecord();
+                } else {
+                    stopRecord();
+                }
+            }
+        });
+    }
+
+    private void initPreviewer() {
+
+        BaseProduct product = DJIServiceApplication.getProductInstance();
+
+        if (product == null || !product.isConnected()) {
+            showToast(getString(R.string.disconnected));
+        } else {
+            if (null != mVideoSurface) {
+                mVideoSurface.setSurfaceTextureListener(this);
+            }
+            if (!product.getModel().equals(Model.UNKNOWN_AIRCRAFT)) {
+                VideoFeeder.getInstance().getPrimaryVideoFeed().setCallback(mReceivedVideoDataCallBack);
+            }
+        }
+    }
+
+    private void uninitPreviewer() {
+        Camera camera = DJIServiceApplication.getCameraInstance();
+        if (camera != null){
+            // Reset the callback
+            VideoFeeder.getInstance().getPrimaryVideoFeed().setCallback(null);
+        }
+    }
+
+    @Override
+    public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+        Log.e(TAG, "onSurfaceTextureAvailable");
+        if (mCodecManager == null) {
+            mCodecManager = new DJICodecManager(this, surface, width, height);
+        }
+    }
+
+    @Override
+    public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+        Log.e(TAG, "onSurfaceTextureSizeChanged");
+    }
+
+    @Override
+    public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+        Log.e(TAG,"onSurfaceTextureDestroyed");
+        if (mCodecManager != null) {
+            mCodecManager.cleanSurface();
+            mCodecManager = null;
+        }
+
+        return false;
+    }
+
+    @Override
+    public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+    }
+
+    public void showToast(final String msg) {
+        runOnUiThread(new Runnable() {
+            public void run() {
+                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.btn_capture:{
+                captureAction();
+                break;
+            }
+            case R.id.btn_shoot_photo_mode:{
+                switchCameraMode(SettingsDefinitions.CameraMode.SHOOT_PHOTO);
+                break;
+            }
+            case R.id.btn_record_video_mode:{
+                switchCameraMode(SettingsDefinitions.CameraMode.RECORD_VIDEO);
+                break;
+            }
+            default:
+                break;
+        }
+    }
+
+    private void switchCameraMode(SettingsDefinitions.CameraMode cameraMode){
+
+        Camera camera = DJIServiceApplication.getCameraInstance();
+        if (camera != null) {
+            camera.setMode(cameraMode, new CommonCallbacks.CompletionCallback() {
                 @Override
-                public void run() {
-                    showToast("registering, pls wait...");
-                    DJISDKManager.getInstance().registerApp(MainActivity.this.getApplicationContext(), new DJISDKManager.SDKManagerCallback() {
-                        @Override
-                        public void onRegister(DJIError djiError) {
-                            if (djiError == DJISDKError.REGISTRATION_SUCCESS) {
-                                showToast("Register Success");
-                                DJISDKManager.getInstance().startConnectionToProduct();
-                            } else {
-                                showToast("Register sdk fails, please check the bundle id and network connection!");
-                            }
-                            Log.v(TAG, djiError.getDescription());
-                        }
-                        @Override
-                        public void onProductDisconnect() {
-                            Log.d(TAG, "onProductDisconnect");
-                            showToast("Product Disconnected");
-                            notifyStatusChange();
-                        }
-                        @Override
-                        public void onProductConnect(BaseProduct baseProduct) {
-                            Log.d(TAG, String.format("onProductConnect newProduct:%s", baseProduct));
-                            showToast("Product Connected");
-                            notifyStatusChange();
-                        }
-                        @Override
-                        public void onComponentChange(BaseProduct.ComponentKey componentKey, BaseComponent oldComponent,
-                                                      BaseComponent newComponent) {
-                            if (newComponent != null) {
-                                newComponent.setComponentListener(new BaseComponent.ComponentListener() {
-                                    @Override
-                                    public void onConnectivityChange(boolean isConnected) {
-                                        Log.d(TAG, "onComponentConnectivityChanged: " + isConnected);
-                                        notifyStatusChange();
-                                    }
-                                });
-                            }
-                            Log.d(TAG,
-                                    String.format("onComponentChange key:%s, oldComponent:%s, newComponent:%s",
-                                            componentKey,
-                                            oldComponent,
-                                            newComponent));
-                        }
-                    });
+                public void onResult(DJIError error) {
+
+                    if (error == null) {
+                        showToast("Switch Camera Mode Succeeded");
+                    } else {
+                        showToast(error.getDescription());
+                    }
                 }
             });
         }
     }
 
-    private void notifyStatusChange() {
-        mHandler.removeCallbacks(updateRunnable);
-        mHandler.postDelayed(updateRunnable, 500);
-    }
-    private Runnable updateRunnable = new Runnable() {
-        @Override
-        public void run() {
-            Intent intent = new Intent(FLAG_CONNECTION_CHANGE);
-            sendBroadcast(intent);
+    // Method for taking photo
+    private void captureAction(){
+
+        final Camera camera = DJIServiceApplication.getCameraInstance();
+        if (camera != null) {
+
+            SettingsDefinitions.ShootPhotoMode photoMode = SettingsDefinitions.ShootPhotoMode.SINGLE; // Set the camera capture mode as Single mode
+            camera.setShootPhotoMode(photoMode, new CommonCallbacks.CompletionCallback(){
+                @Override
+                public void onResult(DJIError djiError) {
+                    if (null == djiError) {
+                        handler.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                camera.startShootPhoto(new CommonCallbacks.CompletionCallback() {
+                                    @Override
+                                    public void onResult(DJIError djiError) {
+                                        if (djiError == null) {
+                                            showToast("take photo: success");
+                                        } else {
+                                            showToast(djiError.getDescription());
+                                        }
+                                    }
+                                });
+                            }
+                        }, 2000);
+                    }
+                }
+            });
         }
-    };
-    private void showToast(final String toastMsg) {
-        Handler handler = new Handler(Looper.getMainLooper());
-        handler.post(new Runnable() {
-            @Override
-            public void run() {
-                Toast.makeText(getApplicationContext(), toastMsg, Toast.LENGTH_LONG).show();
-            }
-        });
+    }
+
+    // Method for starting recording
+    private void startRecord(){
+
+        final Camera camera = DJIServiceApplication.getCameraInstance();
+        if (camera != null) {
+            camera.startRecordVideo(new CommonCallbacks.CompletionCallback(){
+                @Override
+                public void onResult(DJIError djiError)
+                {
+                    if (djiError == null) {
+                        showToast("Record video: success");
+                    }else {
+                        showToast(djiError.getDescription());
+                    }
+                }
+            }); // Execute the startRecordVideo API
+        }
+    }
+
+    // Method for stopping recording
+    private void stopRecord(){
+
+        Camera camera = DJIServiceApplication.getCameraInstance();
+        if (camera != null) {
+            camera.stopRecordVideo(new CommonCallbacks.CompletionCallback(){
+
+                @Override
+                public void onResult(DJIError djiError)
+                {
+                    if(djiError == null) {
+                        showToast("Stop recording: success");
+                    }else {
+                        showToast(djiError.getDescription());
+                    }
+                }
+            }); // Execute the stopRecordVideo API
+        }
+
     }
 }
